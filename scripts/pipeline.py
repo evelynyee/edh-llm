8071ef8a260e01f9ce8fa126bbcd83ec67acb2e6
Theo Hui <tchui@ucsd.edu>
2024-03-12 14:17:11 -0700

    test_results

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -86,12 +86,12 @@ def main():
 
     cards_clean, commander_texts, card_texts = clean_data(cards, commanders)
     
-    model = train_model(cards_clean)
+    #model = train_model(cards_clean)
 
-    results_base, results_base_all = baseline(card_texts, commander_texts, model)
-    results_manual = manual(card_texts, commander_texts, model, results_base_all)
-    save_decks(results_base, BASE_PATH)
-    save_decks(results_manual, MANUAL_PATH)
+    #results_base, results_base_all = baseline(card_texts, commander_texts, model)
+    #results_manual = manual(card_texts, commander_texts, model, results_base_all)
+    #save_decks(results_base, BASE_PATH)
+    #save_decks(results_manual, MANUAL_PATH)
 
     build_decks(commander_texts)
 
